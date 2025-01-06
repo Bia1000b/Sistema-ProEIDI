9c86fc3000f5c95ce49998ed0165d8c1889292a6
Bianca Medeiros <biancamm0107@gmail.com>
2025-01-06 19:18:23 -0300

    projectSkeleton

---
+++ src/principal/Main.java
@@ -0,0 +1,8 @@
+package src.principal;
+
+class Main{
+    public static void main(String[] args) {
+        //add
+
+    }
+}
---
+++ src/utils/Pessoa.java
@@ -0,0 +1,14 @@
+package src.utils;
+
+import src.enums.Genero;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public abstract class Pessoa implements Serializable {
+//    private String nome;
+//    private String cpf;
+//    private Genero genero;
+//    private String numeroDeCelular;
+//    private Integer numeroFaltas;
+    //todo
+}
