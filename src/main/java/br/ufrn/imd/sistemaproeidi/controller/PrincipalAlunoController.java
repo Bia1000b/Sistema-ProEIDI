9e372d6fa45a37044ebc39370061edfa4f6df367
marina-medeiros <marinamaleite@gmail.com>
2025-01-14 14:44:55 -0300

    Display ArrayList

--- src/main/java/br/ufrn/imd/sistemaproeidi/controller/PrincipalAlunoController.java
+++ src/main/java/br/ufrn/imd/sistemaproeidi/controller/PrincipalAlunoController.java
@@ -1,5 +1,8 @@
 package br.ufrn.imd.sistemaproeidi.controller;
 
+import br.ufrn.imd.sistemaproeidi.model.enums.Curso;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +14,7 @@ import javafx.stage.Stage;
 public class PrincipalAlunoController {
     @FXML private TabPane tabPane;
     @FXML private Label nomeUsuario;
+    @FXML private ListView<String> cursosListView;
     @FXML private Tab perfilTab, turmaTab;
     @FXML private Button btn_perfil, btn_turma,btn_perfil1, btn_turma1;
     private Aluno aluno;
@@ -36,6 +40,19 @@ public class PrincipalAlunoController {
         if (aluno != null) {
             nomeUsuario.setText(aluno.getNome()); // Atualize o nome no Label
             System.out.println("Dados do aluno carregados: " + aluno.getNome());
+        }
+    }
+
+    private void carregarCursosFeitos(){
+        if (aluno.getCursosFeitos() != null) {
+            // Convertendo os cursos para uma lista de strings para exibir na ListView
+            ObservableList<String> cursos = FXCollections.observableArrayList();
+
+            for (Curso curso : aluno.getCursosFeitos()) {
+                cursos.add(curso.getNome()); // Adicione o nome do curso ou outras informações desejadas
+            }
+
+            cursosListView.setItems(cursos); // Configura os cursos na ListView
         }
     }
 
