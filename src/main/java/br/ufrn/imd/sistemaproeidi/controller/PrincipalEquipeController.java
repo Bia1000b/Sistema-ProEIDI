37c6353a2243bc18d9d40af30c9a3c186b8b7d97
Bianca Medeiros <biancamm0107@gmail.com>
2025-01-14 17:39:49 -0300

    PrincipalEquipe template

--- src/main/java/br/ufrn/imd/sistemaproeidi/controller/PrincipalEquipeController.java
+++ src/main/java/br/ufrn/imd/sistemaproeidi/controller/PrincipalEquipeController.java
@@ -1,12 +1,19 @@
 package br.ufrn.imd.sistemaproeidi.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class PrincipalEquipeController {
-    @FXML
-    private TabPane tabPane;
+
+    @FXML private TabPane tabPane;
+    @FXML private Label nomeUsuario;
+    @FXML private Tab perfilTab, turmasTab, buscarTab,cadastrarAlunoTab,cadastrarEquipeTab,chamadaTab;
+    @FXML private Button btn_perfil, btn_turmas, btn_cadastrarAluno,btn_cadastrarEquipe, btn_chamada, btn_buscar;
 
     private Stage principalSceneEquipe;
 
@@ -14,4 +21,44 @@ public class PrincipalEquipeController {
         this.principalSceneEquipe = stage;
     }
 
+    @FXML
+    public void initialize() {
+        System.out.println("Tela Principal Equipe carregada!");
+    }
+
+    @FXML
+    void clicarBtnPerfil(ActionEvent event) {
+        tabPane.getSelectionModel().select(perfilTab);
+        System.out.println("Botão PERFIL clicado.");
+    }
+
+    @FXML
+    void clicarBtnTurmas(ActionEvent event) {
+        tabPane.getSelectionModel().select(turmasTab);
+        System.out.println("Botão TURMAS clicado.");
+    }
+
+    @FXML
+    void clicarBtnCadastrarAluno(ActionEvent event) {
+        tabPane.getSelectionModel().select(cadastrarAlunoTab);
+        System.out.println("Botão CADASTRAR ALUNO clicado.");
+    }
+
+    @FXML
+    void clicarBtnCadastrarEquipe(ActionEvent event) {
+        tabPane.getSelectionModel().select(cadastrarEquipeTab);
+        System.out.println("Botão CADASTRAR EQUIPE clicado.");
+    }
+
+    @FXML
+    void clicarBtnChamada(ActionEvent event) {
+        tabPane.getSelectionModel().select(chamadaTab);
+        System.out.println("Botão CHAMADA clicado.");
+    }
+
+    @FXML
+    void clicarBtnBuscar(ActionEvent event) {
+        tabPane.getSelectionModel().select(buscarTab);
+        System.out.println("Botão BUSCAR clicado.");
+    }
 }
