7f5dfc9c56856c3dc00087741705799b4981a4a6
marina-medeiros <marinamaleite@gmail.com>
2025-01-15 17:36:41 -0300

    ufa!

--- src/main/java/br/ufrn/imd/sistemaproeidi/controller/PrincipalEquipeController.java
+++ src/main/java/br/ufrn/imd/sistemaproeidi/controller/PrincipalEquipeController.java
@@ -1,9 +1,16 @@
 package br.ufrn.imd.sistemaproeidi.controller;
 
+import br.ufrn.imd.sistemaproeidi.SistemaApplication;
 import br.ufrn.imd.sistemaproeidi.model.Aluno;
+import br.ufrn.imd.sistemaproeidi.model.BancoDAO;
 import br.ufrn.imd.sistemaproeidi.model.MembroEquipe;
 import br.ufrn.imd.sistemaproeidi.model.Turma;
+import br.ufrn.imd.sistemaproeidi.model.enums.Cargo;
+import br.ufrn.imd.sistemaproeidi.model.enums.Escolaridade;
+import br.ufrn.imd.sistemaproeidi.model.enums.Genero;
+import br.ufrn.imd.sistemaproeidi.model.enums.SistemaOperacional;
 import br.ufrn.imd.sistemaproeidi.utils.InputUtils;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -11,6 +18,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class PrincipalEquipeController {
@@ -25,14 +34,18 @@ public class PrincipalEquipeController {
     @FXML private Button btn_buscar, btn_buscar1, btn_buscar11, btn_buscar111, btn_buscar1111, btn_buscar11111;
     @FXML private TextField cadastroAlunoCPF, cadastroAlunoNome, cadastroAlunoObsSaude, cadastroAlunoTelefone, cadastroEquipeCPF, cadastroEquipeCursoUFRN, cadastroEquipeEmail, cadastroEquipeMatricula, cadastroEquipeNome, cadastroEquipeTelefone;
     @FXML private DatePicker cadastroAlunoDataNascimento;
-    @FXML private ChoiceBox<?> cadastroAlunoEscolaridade, cadastroAlunoGenero, cadastroAlunoSO, cadastroAlunoTurmaDisponiveis, cadastroEquipeCaargo;
+    @FXML private ChoiceBox<Genero> cadastroAlunoGenero;
+    @FXML private ChoiceBox<SistemaOperacional> cadastroAlunoSO;
+    @FXML private ChoiceBox<Escolaridade> cadastroAlunoEscolaridade;
+    @FXML private ChoiceBox<Cargo>  cadastroEquipeCargo;
+    @FXML private ChoiceBox<Turma>  cadastroAlunoTurmaDisponiveis;
     @FXML private CheckBox checkAlunoComputador, checkAlunoInternet, checkAlunoSmartphone;
     @FXML private Label nomeUsuario, nomeUsuario1, cursoUFRN, cursoUFRN1, email, email1, faltas, faltas1, matricula, matricula1, matricula11, numeroDeTelefone, numeroDeTelefone1;
     @FXML private Pane inner_pane2, inner_pane21, inner_pane211, inner_pane2111, inner_pane21111, inner_pane211111, pane_111112, pane_1111121, pane_11111211, pane_111112111, pane_1111121111, pane_11111211111, pane_111112111111;
     @FXML private HBox root2, root21, root211, root2111, root21111, root211111;
 
     private MembroEquipe membroEquipe;
-    private Vector<Turma> turmas;
+    private ArrayList<Turma> turmas = BancoDAO.getInstance().getArrayTurmas();
 
     private Stage principalSceneEquipe;
 
@@ -43,6 +56,12 @@ public class PrincipalEquipeController {
     @FXML
     public void initialize() {
         System.out.println("Tela Principal Equipe carregada!");
+        cadastroAlunoGenero.setItems(FXCollections.observableArrayList(Genero.values()));
+        cadastroAlunoSO.setItems(FXCollections.observableArrayList(SistemaOperacional.values()));
+        cadastroAlunoEscolaridade.setItems(FXCollections.observableArrayList(Escolaridade.values()));
+        cadastroAlunoTurmaDisponiveis.setItems(FXCollections.observableArrayList(turmas));
+        cadastroEquipeCargo.setItems(FXCollections.observableArrayList(Cargo.values()));
+
     }
 
     public void setMembroEquipe(MembroEquipe membroEquipe) {
@@ -74,10 +93,35 @@ public class PrincipalEquipeController {
 
 
 
+    @FXML
+    public void clicarBtnCadastrarAlunoFinal(ActionEvent event) {
+        System.out.println("Botão CADASTRAR ALUNO clicado.");
+        String nome = cadastroAlunoNome.getText();
+        LocalDate dataNascimento = cadastroAlunoDataNascimento.getValue();
+        String cpf = cadastroAlunoCPF.getText();
+        Genero genero = (Genero) cadastroAlunoGenero.getValue();
+        String numeroCelular = cadastroAlunoTelefone.getText();
+        Escolaridade escolaridade = (Escolaridade) cadastroAlunoEscolaridade.getValue();
+        Turma turma = (Turma) cadastroAlunoTurmaDisponiveis.getValue();
+        String obsSaude = cadastroAlunoObsSaude.getText();
+        boolean temInternet = checkAlunoInternet.isSelected();
+        boolean temComputador = checkAlunoComputador.isSelected();
+        boolean temSmartphone = checkAlunoSmartphone.isSelected();
+        SistemaOperacional sistemaOperacional = (SistemaOperacional) cadastroAlunoSO.getValue();
 
+        membroEquipe.matricularAluno(nome, cpf, genero, dataNascimento, numeroCelular, escolaridade, obsSaude, temInternet, temComputador, temSmartphone, sistemaOperacional, turma);
+    }
 
+    @FXML
+    public void clicarBtnCadastrarEquipeFinal(ActionEvent event) {
+        System.out.println("Botão CADASTRAR EQUIPE clicado.");
+        String nome = cadastroEquipeNome.getText();
+        String cpf = cadastroEquipeCPF.getText();
+        String numeroCelular = cadastroAlunoTelefone.getText();
+        Cargo cargo = (Cargo) cadastroEquipeCargo.getValue();
 
-
+        //membroEquipe.cadastrarMembroEquipe(nome, cpf, null, numeroCelular, matricula, cursoUFRN, email, cargo);
+    }
 
     @FXML
     void clicarBtnPerfil(ActionEvent event) {
